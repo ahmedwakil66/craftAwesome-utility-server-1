1c5ce6cc12a72ab517a6b426bed3869aeaaa8ea7
ahmedwakil66 <ahmedwakil66@gmail.com>
2023-05-05 14:05:54 +0600

    simply recipes data added

--- index.js
+++ index.js
@@ -2,14 +2,83 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 const cors = require('cors');
+
 const dragonNews = require('./dragon-news-data/news.json');
 const dragonCategories = require('./dragon-news-data/categories.json');
+
+const chefsData = require('./simply-recipes-data/chef_data.json');
+const recipesData = require('./simply-recipes-data/recipes_data.json');
+const qAndA = require('./simply-recipes-data/q&a_data.json');
+
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('hi, welcome to craftAwesome utility server 1!');
 })
+
+
+
+//===========================
+//==== Simply Recipes Starts ===
+//===========================
+
+//send all chef data
+app.get('/simply-recipes/chefs', (req, res) => {
+    res.send(chefsData);
+})
+
+
+//building category data from all recipe and sending it
+app.get('/simply-recipes/categories', (req, res) => {
+    const categories = [];
+    const catData = [];
+    recipesData.forEach(recipe => {
+        const recipeCat = recipe.category;
+        if(categories.indexOf(recipeCat) === -1){
+            categories.push(recipeCat);
+            catData.push({
+                category: recipe.category,
+                image: recipe.image
+            });
+        }
+    });
+    res.send(catData);
+})
+
+
+//send all recipes for a specific category
+app.get('/simply-recipes/categories/:catName', (req, res) => {
+    const catName = req.params.catName;
+    const categoryItems = recipesData.filter(recipe => recipe.category === catName) || [];
+    res.send(categoryItems);
+})
+
+
+//send specific recipe data
+app.get('/simply-recipes/chef/:chefId', (req, res) => {
+    const id = req.params.chefId;
+    const chef = chefsData.find(chef => chef._id === id);
+    const chefRecipes = recipesData.filter(recipe => recipe.chef_id === id) || [];
+    res.send({chef, chefRecipes});
+})
+
+
+//send today's pick
+app.get('/simply-recipes/todays-pick', (req, res) => {
+    res.send(recipesData.slice(0 ,1));
+})
+
+
+//send all q&a data
+app.get('/simply-recipes/q-and-a', (req, res) => {
+    res.send(qAndA);
+})
+//===========================
+//==== Simply Recipes Ends ===
+//===========================
+
+
 
 //===========================
 //==== Dragon News Starts ===
@@ -63,5 +132,5 @@ app.get('/dragon-news-todays-pick', (req, res) =>{
 
 
 app.listen(port, () => {
-    console.log('craftAwesome utility server 1 has been started...');
+    console.log(`craftAwesome utility server 1 has been started to port ${port}...`);
 })
